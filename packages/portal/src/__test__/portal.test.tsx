12dbcc562acb25546a9912bb1d12a994f91c372b
osman.akar <osman.akar@ciceksepeti.com>
2021-10-14 00:21:16 +0300

    feat: test implementations for portal package

--- packages/portal/src/__test__/portal.test.tsx
+++ packages/portal/src/__test__/portal.test.tsx
@@ -1,3 +1,45 @@
-test('utils test', () => {
-  expect(5).toBe(5);
+import React, { useRef } from 'react';
+import Portal from '..';
+import { render, cleanup, within } from '@cs/component-utils';
+
+describe('Portal component tests', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('expect portal to render inside of specified element by id', () => {
+    const { getByTestId } = render(<ById />);
+    const { getByText } = within(getByTestId('container'));
+
+    getByText(/i am in a portal/i);
+  });
+
+  test('expect portal to render inside of specified element by ref', () => {
+    const { getByTestId } = render(<ByRef />);
+    const { getByText } = within(getByTestId('container'));
+
+    getByText(/i am in a portal/i);
+  });
 });
+
+const ById = () => (
+  <div>
+    <div data-testid="container" id="container"></div>
+    <Portal containerId="container">
+      <p>I am in a portal</p>
+    </Portal>
+  </div>
+);
+
+const ByRef = () => {
+  const ref = useRef();
+
+  return (
+    <div>
+      <div data-testid="container" ref={ref}></div>
+      <Portal containerRef={ref}>
+        <p>I am in a portal</p>
+      </Portal>
+    </div>
+  );
+};
