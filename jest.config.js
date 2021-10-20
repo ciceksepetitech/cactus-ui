94063275dca09792c38b384623156d49c95aeec6
osman.akar <osman.akar@ciceksepeti.com>
2021-10-20 15:29:31 +0300

    feat: release.yml added

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,22 @@
 module.exports = {
   roots: ['<rootDir>/packages'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
   cacheDirectory: '.jest-cache',
   testEnvironment: 'jsdom',
   coverageDirectory: '.jest-coverage',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>jest/setupTests.ts'],
-  coveragePathIgnorePatterns: ['node_modules', '.stories.tsx', '.stories.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  coveragePathIgnorePatterns: [
+    '/dist/',
+    '.stories.ts',
+    'node_modules',
+    '.stories.tsx'
+  ],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': '<rootDir>/__test__/jest/__mocks__/styleMock.js'
   },
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  }
+  coverageThreshold: null
 };
