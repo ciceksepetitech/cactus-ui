5bcc322a7a7132efaee0be22a246649774db9151
osman <osman@192.168.1.48>
2021-12-16 01:28:28 +0300

    feat: usePopover implementations

--- packages/popover/src/index.tsx
+++ packages/popover/src/index.tsx
@@ -6,62 +6,156 @@
  */
 
 import Portal from '@cs/component-portal';
-import { useCombinedRefs } from '@cs/component-hooks';
+import { useCombinedRefs, useEventListener } from '@cs/component-hooks';
 import React, {
   useRef,
+  useState,
   forwardRef,
   useCallback,
+  CSSProperties,
+  useLayoutEffect,
   MutableRefObject
 } from 'react';
 import {
-  isCSR,
   ConditionalWrapper,
   PolymorphicComponentProps
 } from '@cs/component-utils';
 
 const usePopover = ({
+  autoFlip,
   placement,
   targetRef,
-  popoverRef,
-  bounding = isCSR ? window : undefined
-}) => {
-  const getTargetNodeRect = useCallback((nodeRef) => {
-    return nodeRef.current.getBoundingClientRect();
-  }, []);
+  popoverRef
+}: IUsePopoverProps) => {
+  const [styles, setStyles] = useState<CSSProperties>({
+    position: 'absolute',
+    visibility: 'hidden'
+  });
 
-  const getTargetNodeDistances = useCallback((nodeRef) => {
-    const { top, bottom, left, right } =
-      nodeRef.current.getBoundingClientRect();
+  const getAvailableSpaces = useCallback(
+    (targetNode: DOMRect, popoverNode: DOMRect): Record<string, boolean> => {
+      const { innerWidth, innerHeight } = window;
+      const { width: pWidth, height: pHeight } = popoverNode;
+      const {
+        top: tTop,
+        left: tLeft,
+        width: tWidth,
+        bottom: tBottom
+      } = targetNode;
 
-    return { top, bottom, left, right };
-  }, []);
+      return {
+        top: tTop - pHeight > 0,
+        left: tLeft - pWidth > 0,
+        bottom: innerHeight > tBottom + pHeight,
+        right: innerWidth > tLeft + tWidth + pWidth
+      };
+    },
+    []
+  );
 
-  const getPopoverStyles = useCallback(() => {
-    const targetNode = getTargetNodeRect(targetRef);
+  const getPlacementLeft = useCallback(
+    (
+      targetNode: DOMRect,
+      popoverNode: DOMRect
+    ): Pick<DOMRect, 'left' | 'top'> => {
+      return { left: targetNode.x - popoverNode.width, top: targetNode.y };
+    },
+    []
+  );
 
-    const left = targetNode.y;
-    const top =
-      targetNode.x +
-      (placement === 'bottom' ? targetNode.height : -1 * targetNode.height);
+  const getPlacementRight = useCallback(
+    (targetNode: DOMRect): Pick<DOMRect, 'left' | 'top'> => {
+      return { left: targetNode.x + targetNode.width, top: targetNode.y };
+    },
+    []
+  );
 
-    return {
-      top,
-      left,
-      position: 'absolute'
-    };
-  }, [placement, getTargetNodeRect]);
+  const getPlacementBottom = useCallback(
+    (targetNode: DOMRect): Pick<DOMRect, 'left' | 'top'> => {
+      return { left: targetNode.x, top: targetNode.y + targetNode.height };
+    },
+    []
+  );
+
+  const getPlacementTop = useCallback(
+    (
+      targetNode: DOMRect,
+      popoverNode: DOMRect
+    ): Pick<DOMRect, 'left' | 'top'> => {
+      return {
+        left: targetNode.left,
+        top: targetNode.top - popoverNode.height
+      };
+    },
+    []
+  );
+
+  const getPlacementGetter = useCallback(
+    (placement: OppositePlacements | Placements) => {
+      switch (placement) {
+        case Placements.Top:
+          return getPlacementTop;
+
+        case Placements.Bottom:
+          return getPlacementBottom;
+
+        case Placements.Left:
+          return getPlacementLeft;
+
+        case Placements.Right:
+          return getPlacementRight;
+
+        default:
+          break;
+      }
+    },
+    [getPlacementLeft, getPlacementRight, getPlacementBottom, getPlacementTop]
+  );
+
+  const getPopoverPosition = useCallback(() => {
+    setTimeout(() => {
+      const targetNode: DOMRect = targetRef.current.getBoundingClientRect();
+      const popoverNode: DOMRect = popoverRef.current?.getBoundingClientRect();
+
+      const newPosition: CSSProperties = { visibility: 'visible' };
+      const availableSpaces = getAvailableSpaces(targetNode, popoverNode);
+
+      const shouldFlip =
+        autoFlip &&
+        !availableSpaces[placement] &&
+        availableSpaces[OppositePlacements[placement]];
+
+      const certainPlacement = shouldFlip
+        ? OppositePlacements[placement]
+        : placement;
+
+      const getPlacement = getPlacementGetter(certainPlacement);
+      const { left, top } = getPlacement(targetNode, popoverNode);
+
+      newPosition.top = top;
+      newPosition.left = left;
+
+      setStyles((prev) => ({
+        ...prev,
+        ...newPosition
+      }));
+    }, 0);
+  }, [autoFlip, getAvailableSpaces, getPlacementGetter]);
+
+  useLayoutEffect(() => {
+    getPopoverPosition();
+  }, [getPopoverPosition]);
+
+  useEventListener({ name: 'resize', listener: getPopoverPosition });
+
+  useEventListener({ name: 'scroll', listener: getPopoverPosition });
 
   return {
-    getPopoverStyles,
-    getTargetNodeRect,
-    getTargetNodeDistances
+    styles
   };
 };
 
-/**
- * popover component
- */
-export const Popover = forwardRef(
+const Popover = forwardRef(
   <C extends React.ElementType = 'div'>(
     props: PolymorphicComponentProps<C, IPopoverProps>,
     forwardedRef
@@ -71,8 +165,10 @@ export const Popover = forwardRef(
       style,
       children,
       targetRef,
-      portal = false,
-      placement = 'bottom',
+      portal = true,
+      autoFlip = true,
+      reposition = true,
+      placement = Placements.Bottom,
       ...rest
     } = props;
 
@@ -81,20 +177,24 @@ export const Popover = forwardRef(
     const internalRef = useRef(null);
     const ref = useCombinedRefs(forwardedRef, internalRef);
 
-    const { getPopoverStyles } = usePopover({
+    const { styles: popoverStyles } = usePopover({
+      autoFlip,
       placement,
       targetRef,
       popoverRef: ref
     });
-
-    const _style = { ...getPopoverStyles(), ...style };
 
     return (
       <ConditionalWrapper
         condition={portal}
         wrapper={(children) => <Portal>{children}</Portal>}
       >
-        <Component {...rest} style={_style} ref={ref} data-cs-popover>
+        <Component
+          {...rest}
+          ref={ref}
+          data-cs-popover
+          style={{ ...style, ...popoverStyles }}
+        >
           {children}
         </Component>
       </ConditionalWrapper>
@@ -106,15 +206,33 @@ export default Popover;
 
 /** Types and Interfaces */
 
+enum Placements {
+  Top = 'top',
+  Left = 'left',
+  Right = 'right',
+  Bottom = 'bottom'
+}
+
+enum OppositePlacements {
+  top = Placements.Bottom,
+  left = Placements.Right,
+  right = Placements.Left,
+  bottom = Placements.Top
+}
+
 interface IPopoverProps {
   portal?: boolean;
-  placement?: PlacementType;
+  autoFlip?: boolean;
+  placement?: Placements;
   children: React.ReactNode;
   targetRef: MutableRefObject<any>;
   popoverRef: MutableRefObject<any>;
 }
 
-type PlacementType = 'top' | 'bottom' | 'left' | 'right';
+type IUsePopoverProps = Pick<
+  IPopoverProps,
+  'autoFlip' | 'placement' | 'targetRef' | 'popoverRef'
+>;
 
 /** Display Names */
 
