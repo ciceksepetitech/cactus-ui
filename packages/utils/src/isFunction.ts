e2c773b4fe167811911bda797ae7707816c2db36
Osman Akar <osman.akar@192.168.1.36>
2022-02-04 14:27:35 +0300

    feat: png format of cactus-ui and isFunction type correction

--- packages/utils/src/isFunction.ts
+++ packages/utils/src/isFunction.ts
@@ -1,6 +1,6 @@
 /**
  * @see https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
  *  */
-export const isFunction = (variable): variable is () => any => {
+export const isFunction = (variable): variable is (args: any) => any => {
   return !!(variable && {}.toString.call(variable) == '[object Function]');
 };
