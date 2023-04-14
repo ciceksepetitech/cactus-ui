7b6c01ad56ae1ab794b917efa3cc550af5bdd6d0
osman akar <oakar.elk@gmail.com>
2023-04-14 17:03:40 +0300

    feat: popover prevent overflow feature added and storybook webpack config changes (#49)
    Co-authored-by: osman akar <osmanakar@cloud.upwork.com>

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,29 +1,13 @@
-const path = require('path');
-const TSConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-
 module.exports = ({ config }) => {
   config.module.rules.push({
-    test: /\.tsx?$/,
-    use: [require.resolve('ts-loader')]
+    test: /\.(ts|tsx)$/,
+    use: [
+      { loader: require.resolve('ts-loader') },
+      { loader: require.resolve('react-docgen-typescript-loader') }
+    ]
   });
 
   config.resolve.extensions.push('.ts', '.tsx');
-
-  config.resolve.plugins = config.resolve.plugins || [];
-
-  config.resolve.plugins.push(
-    new TSConfigPathsPlugin({
-      configFile: path.join(__dirname, 'tsconfig.json')
-    })
-  );
-
-  /**
-   * suppress verbose build output of ProgressPlugin
-   * @see https://github.com/storybookjs/storybook/issues/1872#issuecomment-335530526
-   */
-  config.plugins = config.plugins.filter(
-    ({ constructor }) => constructor.name !== 'ProgressPlugin'
-  );
 
   return config;
 };
