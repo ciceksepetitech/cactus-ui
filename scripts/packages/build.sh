2ccf0e9091a35d85a722f0c501a54939dc0d8907
Emir Akar <emir.akar@csadmins-MacBook-Pro.local>
2025-11-18 23:54:57 +0300

    [fix] CWF-507 remove scripty and use npm scripts

--- scripts/packages/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "┏━━━ 📦 Packages are building ━━━━━━━━━━━━━━━━━━━"
-yarn cache clean && yarn compile
--- scripts/packages/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo "┏━━━ 📦 Packages are cleaning ━━━━━━━━━━━━━━━━━━━"
-rimraf ./dist && mkdirp ./dist
