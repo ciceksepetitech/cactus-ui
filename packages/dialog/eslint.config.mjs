8b537e62b4f0d1b63b0e0fef896bcb6f9b09f4e3
Emir Akar <emir.akar@csadmins-MacBook-Pro.local>
2025-11-21 17:23:12 +0300

    [chore] migrate eslint configurations

---
+++ packages/dialog/eslint.config.mjs
@@ -0,0 +1,13 @@
+import rootConfig from '../../eslint.config.mjs';
+
+export default [
+  ...rootConfig,
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    }
+  }
+];
